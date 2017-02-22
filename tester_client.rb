72c7264176a5991899e61c6fcf1399a88392f956
Daniel Seehausen <daniel.seehausen@gmail.com>
2017-02-22 11:31:10 -0500

    tester client added

---
+++ tester_client.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'websocket-client-simple'
+
+url = ARGV.shift || 'ws://localhost:8080'
+
+ws = WebSocket::Client::Simple.connect url
+
+ws.on :message do |msg|
+  puts msg.data
+end
+
+ws.on :open do
+  ws.send 'hello!!!'
+end
+
+ws.on :close do |e|
+  p e
+  exit 1
+end
+
+ws.on :error do |e|
+  p e
+end
+
+loop do
+  ws.send STDIN.gets.strip
+end
